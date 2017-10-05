4ac7fd4095764482ff11a5e5efcdce3e7c52c70a
Encho Enevski <encho.enevski@gmail.com>
2017-10-05 13:06:57 +0300

    added role service and repository

---
+++ BS/RoleService.cs
@@ -0,0 +1,13 @@
+﻿using BS.Configs;
+using Interfaces.BS;
+using Interfaces.Repository;
+
+namespace BS
+{
+    public class RoleService : BaseService, IBaseService, IRoleService
+    {
+        public RoleService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
---
+++ Repository/RoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using DB;
+using Interfaces.Repository;
+using Repository.Configs;
+
+namespace Repository
+{
+    public class RoleRepository : GenericRepository<Role>, IGenericRepository<Role>, IRoleRepository
+    {
+        public RoleRepository(AppraisalDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
