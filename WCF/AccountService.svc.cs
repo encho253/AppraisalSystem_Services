ddd0893d3090093db96389065be7cfea18280940
Encho Enevski <encho.enevski@gmail.com>
2017-10-09 17:39:18 +0300

    Added some changes in AccountWcf Service

--- WCF/AccountService.svc.cs
+++ WCF/AccountService.svc.cs
@@ -1,42 +1,41 @@
 ﻿using BS.Configs;
 using Interfaces.BS;
-using Interfaces.Repository;
 using Interfaces.WCF;
 
 namespace WCF
 {
-    public class AccountService /*: IAccountWcfService*/
+    public class AccountService : IAccountWcfService
     {
-        //public bool ValidateUser(string email, string password)
-        //{
-        //    using (IUnityManagerModule dataContainer = new UnityManagerModule())
-        //    {
-        //        dataContainer.Init();
-        //        ILoginService customerLoginService = dataContainer.Resolve<ILoginService>();
-        //        return customerLoginService.ValidateUser(email, password);
-        //    }
-        //}
+        public bool ValidateUser(string email, string password)
+        {
+            using (IUnityManagerModule dataContainer = new UnityManagerModule())
+            {
+                dataContainer.Init();
+                ILoginService customerLoginService = dataContainer.Resolve<ILoginService>();
+                return customerLoginService.ValidateUser(email, password);
+            }
+        }
 
-        //public void CreateUser(int id, string firstName, string lastName, string password, string email, int roleId)
-        //{
-        //    using (IUnityManagerModule dataContainer = new UnityManagerModule())
-        //    {
-        //        dataContainer.Init();
-        //        IRegisterService customerRegisterService = dataContainer.Resolve<IRegisterService>();
-        //        customerRegisterService.CreateUser(id, firstName, lastName, password, email, roleId);
-        //    }
-        //}
+        public void CreateUser(int id, string firstName, string lastName, string password, string email, int roleId)
+        {
+            using (IUnityManagerModule dataContainer = new UnityManagerModule())
+            {
+                dataContainer.Init();
+                IRegisterService customerRegisterService = dataContainer.Resolve<IRegisterService>();
+                customerRegisterService.CreateUser(id, firstName, lastName, password, email, roleId);
+            }
+        }
 
-        //public string[] GetRolesForUser(string userName)
-        //{
-        //    using (IUnityManagerModule dataContainer = new UnityManagerModule())
-        //    {
-        //        dataContainer.Init();
-        //        IUserService customerUserService = dataContainer.Resolve<IUserService>();
-        //        string[] roles = customerUserService.GetRolesForUser(userName);
+        public string[] GetRolesForUser(string userName)
+        {
+            using (IUnityManagerModule dataContainer = new UnityManagerModule())
+            {
+                dataContainer.Init();
+                IUserService customerUserService = dataContainer.Resolve<IUserService>();
+                string[] roles = customerUserService.GetRolesForUser(userName);
 
-        //        return roles;
-        //    }
-        //}
+                return roles;
+            }
+        }
     }
 }
